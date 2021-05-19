5d75b4cec8f62379bdf444c71d2a38958b580f6c
hatchcanon <nccho2@illinois.edu>
2021-05-19 14:45:07 -0500

    Got vc list working

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # hatch-cogs
 
 <img src="https://img.shields.io/badge/Made%20for-Red%20v3-red?logo=discord" alt="Made for Red">
+
+redbot hatchtest --dev
--- teamgenerator/teamgenerator.py
+++ teamgenerator/teamgenerator.py
@@ -1,11 +1,32 @@
+import discord
 from redbot.core import commands
 
 class teamgenerator(commands.Cog):
-    """My custom cog"""
+    """Team Generator"""
 
     @commands.command()
     async def mycom(self, ctx):
         await ctx.send("I can do stuff!")
 
-    async def get_vc(self, ctx, user, channel):
-        await ctx.send("crap")
+    @commands.command()
+    async def getvc(self, ctx):
+        # author = ctx.message.author
+        vc = ctx.author.voice.channel
+        people = vc.members
+        #em = discord.Embed(title="Team Generated", color=ctx.author.color)
+        #em.add_field(name = "team", value = (people))
+        await ctx.send(people)
+
+    @commands.command()
+    async def getmembers(self, ctx):
+        if ctx.author.voice and ctx.author.voice.channel:
+            vc = ctx.author.voice.channel
+            people = (str(i) for i in vc.members)
+            em = discord.Embed(title="Team Generated", color=ctx.author.color)
+            em.add_field(name="team 1", value=("\n".join([str(i) for i in vc.members])))
+            em.add_field(name="team 2", value=("blank"))
+            await ctx.send(embed=em)
+        else:
+            await ctx.send(f"**You are not connected to a VC, {ctx.author.mention}**")
+
+    #totalvcusers = len(ctx.author.voice.channel.members)
