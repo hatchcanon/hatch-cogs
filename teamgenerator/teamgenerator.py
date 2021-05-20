a39fa0564cdb3bf1bb0c0b56810b8ab02b4ef1e4
hatchcanon <nccho2@illinois.edu>
2021-05-20 00:05:25 -0500

    add map selection

--- teamgenerator/teamgenerator.py
+++ teamgenerator/teamgenerator.py
@@ -23,16 +23,29 @@ class teamgenerator(commands.Cog):
     #     await ctx.send(people)
 
     @commands.command()
-    async def team(self, ctx, val = none):
+    async def team(self, ctx, game=None):
+
+        em = discord.Embed(title="Team Generated", color=ctx.author.color)
+
+        if game is None:
+            pass
+        else:
+            if game == "val":
+                maps = ['Blind', 'Spit', 'Accent', 'Heaven', 'Icebox', 'Breeze']
+                random_index = random.randrange(len(maps))
+                valmaps = maps[random_index]
+                em.add_field(name="Map", value=(valmaps), inline=False)
+            if game == "csgo":
+                maps = ['Inferno', 'Mirage', 'Nuke', 'Overpass', 'Dust II', 'Vertigo', 'Ancient']
+                random_index = random.randrange(len(maps))
+                valmaps = maps[random_index]
+                em.add_field(name="Map", value=(valmaps), inline=False)
+
         if ctx.author.voice and ctx.author.voice.channel:
             vc = ctx.author.voice.channel
             people = [formatname(i) for i in vc.members]
             random.shuffle(people)
             half = math.ceil(len(people) / 2)
-            maps = ['Blind', 'Spit', 'Accent', 'Heaven', 'Icebox', 'Breeze']
-            random_index = random.randrange(len(maps))
-            em = discord.Embed(title="Team Generated", color=ctx.author.color)
-            em.add_field(name="Map", value=(maps[random_index]), inline=False)
             em.add_field(name="Team 1", value=("\n".join(people[:half])), inline=True)
             em.add_field(name="Team 2", value=("\n".join(people[half:])), inline=True)
             await ctx.send(embed=em)
