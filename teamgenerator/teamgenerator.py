0aea3a700afcf8214a464c0d75af735d682e5df1
hatchcanon <nccho2@illinois.edu>
2021-05-19 19:04:32 -0500

    randomized people

--- teamgenerator/teamgenerator.py
+++ teamgenerator/teamgenerator.py
@@ -1,32 +1,40 @@
 import discord
+import random
+import math
 from redbot.core import commands
 
 class teamgenerator(commands.Cog):
     """Team Generator"""
 
+    def formatname(self):
+        return f"> {self.mention}"
+
     @commands.command()
     async def mycom(self, ctx):
         await ctx.send("I can do stuff!")
 
-    @commands.command()
-    async def getvc(self, ctx):
-        # author = ctx.message.author
-        vc = ctx.author.voice.channel
-        people = vc.members
-        #em = discord.Embed(title="Team Generated", color=ctx.author.color)
-        #em.add_field(name = "team", value = (people))
-        await ctx.send(people)
+    # @commands.command()
+    # async def getvc(self, ctx):
+    #     author = ctx.message.author
+    #     vc = ctx.author.voice.channel
+    #     people = [str(i['nick']) or str(i['name']) for i in vc.members]
+    #     em = discord.Embed(title="Team Generated", color=ctx.author.color)
+    #     em.add_field(name = "team", value = (people))
+    #     await ctx.send(people)
 
     @commands.command()
     async def getmembers(self, ctx):
         if ctx.author.voice and ctx.author.voice.channel:
             vc = ctx.author.voice.channel
-            people = (str(i) for i in vc.members)
+            people = [i.mention for i in vc.members]
+            random.shuffle(people)
+            half = math.ceil(len(people) / 2)
+            maps = ['Blind', 'Spit', 'Accent', 'Heaven', 'Icebox', 'Breeze']
+            random_index = random.randrange(len(maps))
             em = discord.Embed(title="Team Generated", color=ctx.author.color)
-            em.add_field(name="team 1", value=("\n".join([str(i) for i in vc.members])))
-            em.add_field(name="team 2", value=("blank"))
+            em.insert_field_at(index=1, name="Map", value=(maps[random_index]))
+            em.add_field(name="Team 1", value=("\n".join(people[:half])))
+            em.add_field(name="Team 2", value=("\n".join(people[half:])))
             await ctx.send(embed=em)
         else:
             await ctx.send(f"**You are not connected to a VC, {ctx.author.mention}**")
-
-    #totalvcusers = len(ctx.author.voice.channel.members)
